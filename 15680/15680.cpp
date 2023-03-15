fcabf25600d599181362c9e6b6992169fce07b1e
psh2428 <simon2428@naver.com>
2023-03-15 16:14:47 +0900

    초기 커밋.

---
+++ 15680/15680.cpp
@@ -0,0 +1,13 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+void main() {
+	int n;
+	scanf("%d", &n);
+	if (n == 0) {
+		printf("YONSEI");
+	}
+	else if (n == 1) {
+		printf("Leading the WAY to the Future");
+	}
+}
